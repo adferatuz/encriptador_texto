3137d57fc1093b37fe45daac5606e242b36fa59e
adferatuz <viadfeur@gmail.com>
2024-03-01 11:39:40 -0500

    version 1.0.1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let contenidoModal = document.getElementById('modalContenido');
 let modal = document.getElementById("miModal");
 
 //declaramos variables
-let texto, textoEncriptado, textoDesencriptado, textoCopiado,elementosModal, botonPegar;
+let texto,textoEncriptado, textoDesencriptado, textoCopiado,elementosModal, botonPegar, regex;
 
 elementosModal = crearElementos();
 botonPegar = crearBoton()
@@ -49,7 +49,8 @@ function elementoModal(){
   //agregamos la X de cerrar la ventana al modal y el evento de cerrar modal
   contenidoModal.appendChild(elementosModal.spanX);
   elementosModal.spanX.addEventListener('click', function (){
-      modal.style.display = "none";
+    textoEncriptar.value = '';
+    modal.style.display = "none";
   })
 }
 
@@ -70,7 +71,7 @@ function encriptar(textoEncriptar){
 
 //funcion para desencriptar el texto
 function desencriptar(textoDesencriptar){
-  let matrizSustitucion = [["e" , "enter"],["i" , "imes"], ["a" , "ai" ],["o", "ober"],["u", "ufat"]];                           
+  let matrizSustitucion = [["a" , "ai" ],["e" , "enter"],["i" , "imes"],["o", "ober"],["u", "ufat"]];                           
                              
   textoDesencriptar = textoDesencriptar.toLowerCase();
 
@@ -83,60 +84,110 @@ function desencriptar(textoDesencriptar){
   return textoDesencriptar;
 };
 
+//funcion pegar desde el porta papeles
+function pegarDesdePortapapeles() {
+  navigator.clipboard
+  .readText()
+  .then(
+    (clipText) => (textoEncriptar.value = clipText),
+  );
+
+}
+
+// Evento keydown para verificar la combinación de teclas "Ctrl + V"
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.key === 'v') {
+    // Previene el comportamiento predeterminado de la combinación de teclas "Ctrl + V"
+    event.preventDefault();
+    
+    // Llama a la función para pegar desde el portapapeles
+    pegarDesdePortapapeles();
+  }
+});
+
 //se le añade un evento al botonEncriptar
 botonEncriptar.addEventListener('click', function(){
+  regex =  /^[a-z\s]*$/;
   texto = textoEncriptar.value;
-  textoEncriptado = encriptar(texto);
 
-  textoDesencriptar.value = textoEncriptado;
+  if(!regex.test(texto)){
+    //agregamos la clase modal al contenedor Modal
+    contenidoModal.classList.add('modal-contenido');
   
-  texto ? contenedorImgDesencriptador.classList.add('oculto') :contenedorImgDesencriptador.classList.remove('oculto') ;
-  //deshabilitamos el boton nuevamente
-  botonDesencriptar.disabled = true;
+    //funcion para agregar elementos al modal
+    elementoModal();
 
+    //creamos el mensaje a mostrar en el modal
+    elementosModal.tituloModal.textContent = `El contenido del textarea no acepta mayusculas,  acentos y  tampoco caracteres especiales`;    
 
+    contenidoModal.appendChild(elementosModal.tituloModal);
+
+    //mostramos el contenido
+    modal.style.display = "flex";
+
+  }else{
+    textoEncriptado = encriptar(texto);
+    
+    textoDesencriptar.value = textoEncriptado;
+    
+    texto ? contenedorImgDesencriptador.classList.add('oculto') :contenedorImgDesencriptador.classList.remove('oculto') ;
+    //deshabilitamos el boton nuevamente
+    botonDesencriptar.disabled = true;
+
+  };
 });
 
 //se le añade un evento al botonDesencriptar
 botonDesencriptar.addEventListener('click', function(){
   texto = textoEncriptar.value;
-  textoDesencriptado = desencriptar(texto);  
-  textoDesencriptar.value = textoDesencriptado;
+  textoDesencriptado = desencriptar(texto); 
+  textoDesencriptar.value = textoDesencriptado;    
+  
 });
 
 //se le añade evento al boton copiar
 botonCopiar.addEventListener('click', function(){
 
-  textoCopiado = textoDesencriptar.value;
-  elementosModal.parrafo.textContent = `${textoCopiado} `;
-  console.log(elementosModal.parrafo.textContent)
+  navigator.clipboard.writeText(textoDesencriptar.value)
+  .then(() => {
 
-  if(textoCopiado){
+    textoCopiado = textoDesencriptar.value;
+    elementosModal.parrafo.textContent = `${textoCopiado} `;
+  
+    if(textoCopiado){
+  
+      //agregamos la clase modal al contenedor Modal
+      contenidoModal.classList.add('modal-contenido');
+  
+      //funcion para agregar elementos al modal
+      elementoModal();
+  
+      //creamos el mensaje a mostrar en el modal
+      elementosModal.tituloModal.textContent = `Texto copiado al portapapeles, deseas pegar el texto para desencriptarlo?: `;    
+  
+      contenidoModal.appendChild(elementosModal.tituloModal);
+      contenidoModal.appendChild(elementosModal.parrafo);
+      contenidoModal.appendChild(botonPegar);
+  
+  
+      //mostramos el contenido
+      modal.style.display = "flex";
 
-    //agregamos la clase modal al contenedor Modal
-    contenidoModal.classList.add('modal-contenido');
+      //deshabilitamos el boton nuevamente
+      botonDesencriptar.disabled = false;
+    };
+  })
+  .catch(err => {
+    console.error('Error al copiar al portapapeles:', err);
+  });
 
-    //funcion para agregar elementos al modal
-    elementoModal();
+  
 
-    //creamos el mensaje a mostrar en el modal
-    elementosModal.tituloModal.textContent = `Deseas pegar el siguiente texto?: `;    
-
-    contenidoModal.appendChild(elementosModal.tituloModal);
-    contenidoModal.appendChild(elementosModal.parrafo);
-    contenidoModal.appendChild(botonPegar);
-
-
-    //mostramos el contenido
-    modal.style.display = "flex";
-
-    //deshabilitamos el boton nuevamente
-    botonDesencriptar.disabled = true;
-  };
 })
 
 //se le añade evento al boton pegar
 botonPegar.addEventListener('click', function(){
+
   let contenidoParrafo = document.getElementById('valorModalEncriptado');
   textoDesencriptar.value = '';
 
